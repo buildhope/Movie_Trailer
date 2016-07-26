7040430c07d7571f837c15ce487dfc21a01346ec
Praveen Jugraj <Praveen Jugraj>
2016-07-26 22:37:17 +1000

    feat: add release date

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,39 +2,36 @@
 # Mini-Project: Movies Website
 
 # In this file, you will define instances of the class Movie defined
-# in media.py. After you follow along with Kunal, make some instances
-# of your own!
+# in media.py. 
 
 # After you run this code, open the file fresh_tomatoes.html to
 # see your webpage!
-
-# https://www.udacity.com/course/viewer#!/c-nd000/l-4185678656/e-991358856/m-1013629064
 
 import media
 import fresh_tomatoes
 
 shawshank_redemption = (media.Movie("Shawshank Redemption","Guy escapes from prison",
                                     "https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg",
-                                    "https://www.youtube.com/watch?v=TaYNFrecwpQ"))
+                                    "https://www.youtube.com/watch?v=TaYNFrecwpQ","02/07/1999"))
 
 avatar = (media.Movie("Avatar","Guy goes to another planet",
                       "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
-                      "https://www.youtube.com/watch?v=5PSNL1qE6VY"))
+                      "https://www.youtube.com/watch?v=5PSNL1qE6VY","11/07/1989"))
 
 killbill_1 = (media.Movie("Kill Bill - 1","Lady is pissed off and wants to bite friends",
                           "https://upload.wikimedia.org/wikipedia/en/c/cf/Kill_bill_vol_one_ver.jpg",
-                          "https://www.youtube.com/watch?v=7kSuas6mRpk"))
+                          "https://www.youtube.com/watch?v=7kSuas6mRpk","23/07/2011"))
 
 pk = (media.Movie("PK","Human like Alien","https://upload.wikimedia.org/wikipedia/en/c/c3/PK_poster.jpg",
-                  "https://www.youtube.com/watch?v=SOXWc32k4zA"))
+                  "https://www.youtube.com/watch?v=SOXWc32k4zA","02/08/1995"))
 
 soodhu_kavvum = (media.Movie("Soodhu Kavvum","Amateur Kidnappers",
                              "https://upload.wikimedia.org/wikipedia/en/9/93/Soodhu_kavvum.jpg",
-                             "https://www.youtube.com/watch?v=08Ar821Av-Q"))
+                             "https://www.youtube.com/watch?v=08Ar821Av-Q","12/07/2016"))
 
 special26 = (media.Movie("Special 26","couple guys give cops a run around",
                          "https://upload.wikimedia.org/wikipedia/en/7/7c/Special_26_poster.jpg",
-                         "https://www.youtube.com/watch?v=PiyQb28geOg"))
+                         "https://www.youtube.com/watch?v=PiyQb28geOg","04/07/1991"))
 
 movies = [shawshank_redemption, avatar, killbill_1,pk,soodhu_kavvum,special26]
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -6,19 +6,16 @@
 # class definitions separate from the rest of their code. This also
 # gives you practice importing Python files.
 
-# https://www.udacity.com/course/viewer#!/c-nd000/l-4185678656/m-1013629057
 
 import webbrowser
 
 class Movie():
     # This class provides a way to store movie related information
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, release_date):
         # initialize instance of class Movie
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-
-    def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
+        self.rel_date = release_date
